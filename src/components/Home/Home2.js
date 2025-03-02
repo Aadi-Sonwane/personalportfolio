1f259f2efc3b19df211797c96d73c6c604b0eb5e
Aditya Sonwane <aadus.info@gmail.com>
2025-03-02 10:49:21 +0000

    updated some ofths code

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+// import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/avtar.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
@@ -25,13 +26,13 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Python. </b>
+                <b className="purple"> DevOps, Cloud and Python. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              My field of Interest's are make new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple"> CICD Pipeline </b> and
                 also in areas related to{" "}
                 <b className="purple">
                   Cloud Computing 
@@ -39,17 +40,17 @@ function Home2() {
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              Whenever possible, I also apply my passion for deply application  
+              with <b className="purple">DevOps</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  Modurn Cloud tech
                 </b>
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> AWS, GCP</b>
               </i>
             </p>
           </Col>
